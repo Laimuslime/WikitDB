47d47862d6378a4e7f4ac1ffcb1726e9dfc228a2
Laimuslime <yshi03395@gmail.com>
2026-03-16 18:15:14 +0800

    Update wikitdb.config.js

--- wikitdb.config.js
+++ wikitdb.config.js
@@ -10,18 +10,21 @@ module.exports = {
             URL: "https://deep-forest-club.wikidot.com/",
             ImgURL: "https://deep-forest-club.wdfiles.com/local--files/component%3Atheme/logo.png",
             PARAM: "dfc"
+            WIKIT_ID: "deep-forest-club"
         },
         {
             NAME: "后室IF分站",
             URL: "https://if-backrooms.wikidot.com/",
             ImgURL: "https://laimu.backroomswiki.top/img/Logo.png",
             PARAM: "if"
+            WIKIT_ID: "if-backrooms"
         },
         {
             NAME: "地下黑市",
             URL: "https://ubmh.wikidot.com/",
             ImgURL: "https://ubmh.wdfiles.com/local--files/admin%3Atheme/ubmh.webp",
             PARAM: "ubmh"
+            WIKIT_ID: "ubmh"
         }
     ]
 };
