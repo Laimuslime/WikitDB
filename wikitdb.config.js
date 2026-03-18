fef74bfa6c266864aa3397f38796eac08e537169
Laimuslime <yshi03395@gmail.com>
2026-03-18 18:00:14 +0800

    Update wikitdb.config.js

--- wikitdb.config.js
+++ wikitdb.config.js
@@ -3,6 +3,7 @@ module.exports = {
     SITE_URL: '',
     SITE_SINCE: '2026',
     SITE_AUTHOR: 'WikitDB Team',
+    SUPPORT_WIKI: [
 /*站点添加格式
         {
             NAME: "站点名称",
@@ -12,41 +13,45 @@ module.exports = {
             WIKIT_ID: "Wikit站点里写的站点名称，这里用于筛选作者的站点页面"
         },
         */
-    SUPPORT_WIKI: [
-        {
+    {
             NAME: "深林文学部",
             URL: "https://deep-forest-club.wikidot.com/",
             ImgURL: "https://deep-forest-club.wdfiles.com/local--files/component%3Atheme/logo.png",
             PARAM: "dfc",
-            WIKIT_ID: "deep-forest-club"
+            WIKIT_ID: "deep-forest-club",
+            AUTHOR_TAG: "作者"
         },
         {
             NAME: "后室IF分站",
             URL: "https://if-backrooms.wikidot.com/",
             ImgURL: "https://laimu.backroomswiki.top/img/Logo.png",
             PARAM: "if",
-            WIKIT_ID: "if-backrooms"
+            WIKIT_ID: "if-backrooms",
+            AUTHOR_TAG: "作者"
         },
         {
             NAME: "地下黑市",
             URL: "https://ubmh.wikidot.com/",
             ImgURL: "https://ubmh.wdfiles.com/local--files/admin%3Atheme/ubmh.webp",
             PARAM: "ubmh",
-            WIKIT_ID: "ubmh"
+            WIKIT_ID: "ubmh",
+            AUTHOR_TAG: "作者"
         },
         {
             NAME: "SCP基金会云国分部",
             URL: "https://scp-wiki-cloud.wikidot.com/",
             ImgURL: "https://scp-wiki-cloud.wdfiles.com/local--files/start/scp-cloud-logo.png",
             PARAM: "scp-wiki-cloud",
-            WIKIT_ID: "scp-wiki-cloud"
+            WIKIT_ID: "scp-wiki-cloud",
+            AUTHOR_TAG: "作者"
         },
         {
             NAME: "规则怪谈档案馆",
             URL: "https://rule-wiki.wikidot.com/",
             ImgURL: "https://rule-wiki.wdfiles.com/local--files/component%3Atheme/rule-wiki-new.svg",
             PARAM: "rule",
-            WIKIT_ID: "rule-wiki"
+            WIKIT_ID: "rule-wiki",
+            AUTHOR_TAG: "作者"
         },
     ]
 };
